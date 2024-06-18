e18a9952c6bcff1a658ad4078dbd335d122058f9
이강산 <gangsan1414@gmail.com>
2024-06-18 18:34:18 +0900

    🩹Fix

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,11 +1,9 @@
 import express from "express";
 import { prisma } from "../utils/prisma.util.js";
 import { authRouter } from "./auth.router.js";
-// import { usersRouter } from "./users.router.js";
+import { usersRouter } from "./users.router.js";
 import { menusRouter } from "./menus.router.js";
 import { storesRouter } from "./store.router.js";
-import { resumesRouter } from "./resumes.router.js";
-import { requireAccessToken } from "../middlewares/require-access-token.middleware.js";
 import { UsersRepository } from "../repositories/users.repository.js";
 import { AuthService } from "../services/auth.service.js";
 
@@ -16,9 +14,8 @@ const usersRepository = new UsersRepository(prisma);
 const authService = new AuthService(usersRepository);
 
 apiRouter.use("/auth", authRouter);
-// apiRouter.use("/users", usersRouter);
+apiRouter.use("/users", usersRouter);
 apiRouter.use("/stores/:store_id", menusRouter);
 apiRouter.use("/stores", storesRouter);
-apiRouter.use("/resumes", requireAccessToken(authService), resumesRouter);
 
 export { apiRouter };
