331b7a2bf8350a87429bd699ce0ed44b131d2924
lucetaseo <101718150+lucetaseo@users.noreply.github.com>
2024-06-18 14:31:03 +0900

    update setting
    update setting

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,3 +1,5 @@
+import nodemailer from "nodemailer";
+
 import { HTTP_STATUS } from "../constants/http-status.constant.js";
 import { MESSAGES } from "../constants/message.constant.js";
 
@@ -36,6 +38,66 @@ export class AuthController {
         data: { accessToken },
       });
     } catch (error) {
+      next(error);
+    }
+  };
+
+  // 이메일 인증 controller
+  smtpTransport = nodemailer.createTransport({
+    pool: true,
+    maxConnections: process.env.MAIL_MAX_CONNECTION,
+    service: process.env.MAIL_SERVICE,
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    secure: false,
+    requireTLS: true,
+    auth: {
+      user: process.env.MAIL_AUTH_USER,
+      pass: process.env.MAIL_AUTH_PASS,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  sendAuthEmail = async (req, res, next) => {
+    try {
+      const { email } = req.body;
+      const verificationCode = EmailVerificationUtil.codeIssue();
+      const timestamp = Date.now();
+
+      EmailVerificationUtil.codes[email] = {
+        code: verificationCode,
+        timestamp,
+      };
+
+      // 여기서도 메일 옵션 util 에서 관리 (html은 상수가 아니기 때문)
+      // 다른 방법 mailOptions 를 그냥 함수로 만들어 보기
+      const mailOptions = {
+        from: AUTH_CONSTANT.AUTH_EMAIL.FROM,
+        to: email,
+        subject: AUTH_CONSTANT.AUTH_EMAIL.SUBJECT,
+        html: `
+      <h1>${AUTH_CONSTANT.AUTH_EMAIL.HTML}</h1>
+      <p>${verificationCode}</p>
+      `,
+      };
+
+      // 메일 보내는 로직은 await 빼기
+      smtpTransport.sendMail(mailOptions);
+
+      console.log(EmailVerificationUtil.codes[email].code);
+
+      res.status(HTTP_STATUS.OK).json({
+        status: HTTP_STATUS.OK,
+        message: MESSAGES.AUTH.SIGN_UP.SUCCEED,
+        data: EmailVerificationUtil.codes,
+      });
+    } catch (error) {
+      res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+        status: HTTP_STATUS.INTERNAL_SERVER_ERROR,
+        message: MESSAGES.AUTH.SIGN_UP.FAIL,
+      });
       next(error);
     }
   };
