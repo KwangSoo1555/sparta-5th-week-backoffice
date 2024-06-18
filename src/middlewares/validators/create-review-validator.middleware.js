198bb6327510c5b2cbadfe9362476d5946d02f07
이강산 <gangsan1414@gmail.com>
2024-06-18 23:43:01 +0900

    🎉Feat:리뷰관련JOI미들웨어생성

---
+++ src/middlewares/validators/create-review-validator.middleware.js
@@ -0,0 +1,23 @@
+import Joi from "joi";
+import { MESSAGES } from "../../constants/message.constant.js";
+
+const schema = Joi.object({
+  rating: Joi.string().required().messages({
+    "any.required": MESSAGES.REVIEWS.COMMON.RATING.REQUIRED,
+  }),
+  content: Joi.string().required().messages({
+    "any.required": MESSAGES.REVIEWS.COMMON.CONTENT.REQUIRED,
+  }),
+  imgUrl: Joi.string().required().messages({
+    "any.required": MESSAGES.REVIEWS.COMMON.IMGURL.REQUIRED,
+  }),
+});
+
+export const createReviewValidator = async (req, res, next) => {
+  try {
+    await schema.validateAsync(req.body);
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
