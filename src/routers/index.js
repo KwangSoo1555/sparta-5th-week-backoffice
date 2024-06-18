220b3d3ffc5965b39fb4e674186d3d1eee333194
lucetaseo <101718150+lucetaseo@users.noreply.github.com>
2024-06-18 17:31:58 +0900

    store index 수정
    store index 수정

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { authRouter } from "./auth.router.js";
 import { menusRouter } from "./menus.router.js";
+import { storesRouter } from "./store.router.js";
 import { resumesRouter } from "./resumes.router.js";
 import { prisma } from "../utils/prisma.util.js";
 import { requireAccessToken } from "../middlewares/require-access-token.middleware.js";
@@ -15,6 +16,7 @@ const authService = new AuthService(usersRepository);
 
 apiRouter.use("/auth", authRouter);
 apiRouter.use("/stores/:store_id", menusRouter);
+apiRouter.use("/stores",storesRouter)
 apiRouter.use("/resumes", requireAccessToken(authService), resumesRouter);
 
 export { apiRouter };
