74eacb889199852ddfedc68b00c2de0c466dbc09
Bok Kwang Soo <qhrrhkdtn1555@gmail.com>
2024-06-19 02:05:02 +0900

    fix/머지 충돌 해결 후 재 저장

--- src/middlewares/validators/create-menu-validator.middleware.js
+++ src/middlewares/validators/create-menu-validator.middleware.js
@@ -1,20 +1,6 @@
-// import Joi from "joi";
-// import { MESSAGES } from "../../constants/message.constant.js";
+import Joi from "joi";
+import { MESSAGES } from "../../constants/message.constant.js";
 
-<<<<<<< HEAD
-
-// const schema = Joi.object({
-//     name: Joi.string().required().messages({
-//     "any.required": MESSAGES.MENUS.COMMON.NAME.REQUIRED,
-//   }),
-//   price: Joi.string().required().messages({
-//     "any.required": MESSAGES.MENUS.COMMON.PRICE.REQUIRED,
-//   }),
-//   popularity: Joi.string().required().messages({
-//     "any.required": MESSAGES.MENUS.COMMON.POPULALITY.REQUIRED,
-//   }),
-// });
-=======
 const schema = Joi.object({
   name: Joi.string().required().messages({
     "any.required": MESSAGES.MENUS.COMMON.NAME.REQUIRED,
@@ -26,13 +12,12 @@ const schema = Joi.object({
     "any.required": MESSAGES.MENUS.COMMON.POPULALITY.REQUIRED,
   }),
 });
->>>>>>> a838675d7f355a5f429b1044c1e9d51df9bc4972
 
-// export const createMenuValidator = async (req, res, next) => {
-//   try {
-//     await schema.validateAsync(req.body);
-//     next();
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+export const createMenuValidator = async (req, res, next) => {
+  try {
+    await schema.validateAsync(req.body);
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
