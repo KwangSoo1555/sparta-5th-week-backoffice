ec50b6764ac89fba2abbde04a0013f72f9a20b63
Bok Kwang Soo <qhrrhkdtn1555@gmail.com>
2024-06-19 12:52:18 +0900

    feat/users enum type constant create but, this api has to test later

---
+++ src/constants/user.constant.js
@@ -0,0 +1,17 @@
+export const USERS_CONSTANT = {
+  GRADE: {
+    A: "고마운분",
+    B: "귀한분",
+    C: "더귀한분",
+    D: "천생연분",
+  },
+  ROLE: {
+    USER: "USER",
+    ADMIN: "ADMIN",
+  },
+  PROVIDER: {
+    LOCAL: "LOCAL",
+    KAKAO: "KAKAO",
+    NAVER: "NAVER",
+  },
+};
