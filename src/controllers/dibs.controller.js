557a2db8a291cdd634a699b8ee1e25e563986b62
lucetaseo <101718150+lucetaseo@users.noreply.github.com>
2024-06-20 12:45:33 +0900

    찜 목록 및 삭제 수정(top목록수정예정)
    찜 목록 및 삭제 수정(top목록수정예정)

--- src/controllers/dibs.controller.js
+++ src/controllers/dibs.controller.js
@@ -9,8 +9,8 @@ export class DibsController {
   // 찜 생성
   createDibs = async (req, res, next) => {
     try {
-      const { userId } = req.user;
-      const storeId = parseInt(req.params.store_id, 10);
+      const userId = req.user.userId;
+      const storeId = +req.params.store_id;
 
       const newDibs = await this.dibsService.createDibs(userId, storeId);
 
@@ -24,12 +24,42 @@ export class DibsController {
     }
   };
 
+  // 사용자가 찜한 가게 목록 조회
+  getUserDibs = async (req, res, next) => {
+    try {
+      const userId = req.user.userId;
+    
+    const dibsList = await this.dibsService.findDibsByUser(userId);
+
+    return res.status(HTTP_STATUS.OK).json({
+      status: HTTP_STATUS.OK,
+      message: MESSAGES.DIBS.LIST.SUCCEED,
+      data: dibsList,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+  // 이번 주 찜이 가장 많은 가게 조회
+  getTopDibs = async (req, res, next) => {
+    try {
+      const topDibbedStore = await this.dibsService.getTopDibs();
+      if (topDibbedStore) {
+        res.status(HTTP_STATUS.OK).json(topDibbedStore);
+      } else {
+        res.status(HTTP_STATUS.NOT_FOUND).json({ message: "Store not found" });
+      }
+    } catch (error) {
+      next(error);
+    }
+  };
+
   // 찜 삭제
   deleteDibs = async (req, res, next) => {
     try {
       const { userId } = req.user;
       const storeId = parseInt(req.params.store_id, 10);
-
       const deletedDibs = await this.dibsService.deleteDibs(userId, storeId);
 
       return res.status(HTTP_STATUS.OK).json({
@@ -41,45 +71,4 @@ export class DibsController {
       next(error);
     }
   };
-
-  // 사용자가 찜한 가게 목록 조회
-  getUserDibs = async (userId) => {
-    const dibsList = await this.dibsService.findDibsByUser(userId);
-    
-    const result = await Promise.all(dibsList.map(async (dibs) => {
-      
-
-      if (!store) {
-        throw new Error("Store not found in dibs");
-    }
-        return {
-            id: dibs.id,
-            name: store.name,
-            category: store.category,
-            address: store.address,
-            storePictureUrl: store.storePictureUrl,
-            phone: store.phone,
-            content: store.content,
-            createdAt: dibs.createdAt,
-            updatedAt: dibs.updatedAt,
-        };
-    }));
-
-    return result;
 }
-
-  // 이번 주 찜이 가장 많은 가게 조회
-  getTopDibs = async (req, res, next) => {
-    try {
-        const topDibbedStore = await this.dibsService.getTopDibs();
-        if (topDibbedStore) {
-          res.status(HTTP_STATUS.OK).json(topDibbedStore);
-        } else {
-          res.status(HTTP_STATUS.NOT_FOUND).json({ message: "Store not found" });
-        }
-      } catch (error) {
-        next(error);
-      }
-    };
-}
-
